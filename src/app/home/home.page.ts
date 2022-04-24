42b88b7849c935c28350ced8e67b346b0d37476e
Yoogy Handi Nata <72843811+handinatayoogy@users.noreply.github.com>
2022-04-24 21:23:58 +0700

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  data: any;
   constructor() {}
-
+  ngOnInit() {
+    fetch('assets/json/movies.json')
+      .then((res) => res.json())
+      .then((json) => {
+        this.data = json['results'];
+        console.log(this.data);
+      });
+  }
 }
